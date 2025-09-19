985a74051405c7ce3f7f3dec2cd5f29755e470fa
LailianaMuniz <lailianam@nycstudents.net>
2025-09-19 11:46:21 -0400

    added new file named dogwalker and added it to the main code

---
+++ src/DogWalker.java
@@ -0,0 +1,10 @@
+public class DogWalker
+{
+    private int maxDogs;
+    private DogWalkCompany company;
+    public DogWalker(int max, DogWalkCompany comp)
+    {
+        maxDogs = max;
+        company = comp;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
+import java.util.Scanner;
+
 public class Main
 {
     public static void main(String[] args)
     {
         DogWalkCompany c = new DogWalkCompany();
         System.out.println(c);
+        int hour = c.addDogs();
+        System.out.println(c);
+        DogWalker w = new DogWalker(4, c);
     }
 }
